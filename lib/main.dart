711c8479c78e12ede8e65201f53605fae3d6d1c7
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-23 13:27:35 +0300

    Update main.dart
    update the new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 import 'package:bloc_patterns/counter_app/app.dart';
-import 'package:bloc_patterns/counter_app/counter_observer.dart';
+import 'package:bloc_patterns/timer_app/app.dart';
+//import 'package:bloc_patterns/counter_app/counter_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
-  Bloc.observer = const CounterObserver();
-  runApp(const CounterApp());
-}
+///[CounterApp]
+// void main() {
+//   Bloc.observer = const CounterObserver();
+//   runApp(const CounterApp());
+// }
+
+void main() => runApp(const App());
