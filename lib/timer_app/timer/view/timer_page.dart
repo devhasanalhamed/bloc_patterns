e951b510ebcf4295de822069ca5cb6f8edc7e673
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-24 20:44:15 +0300

    Update timer_page.dart

--- lib/timer_app/timer/view/timer_page.dart
+++ lib/timer_app/timer/view/timer_page.dart
@@ -2,6 +2,8 @@ import 'package:bloc_patterns/timer_app/ticker.dart';
 import 'package:bloc_patterns/timer_app/timer/bloc/timer_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
 class TimerPage extends StatelessWidget {
   const TimerPage({Key? key}) : super(key: key);
@@ -33,9 +35,9 @@ class TimerView extends StatelessWidget {
                 padding: EdgeInsets.symmetric(vertical: 100.0),
                 child: Center(child: TimerText()),
               ),
+              Actions(),
             ],
           ),
-          Actions(),
         ],
       ),
     );
@@ -49,11 +51,11 @@ class TimerText extends StatelessWidget {
   Widget build(BuildContext context) {
     final duration = context.select((TimerBloc bloc) => bloc.state.duration);
     final minutesStr =
-        ((duration / 60) % 60).floor().toString().padLeft(2, '2');
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
     final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
     return Text(
       '$minutesStr: $secondsStr',
-      style: Theme.of(context).textTheme.displayLarge,
+      style: Theme.of(context).textTheme.headlineLarge,
     );
   }
 }
@@ -85,12 +87,22 @@ class Actions extends StatelessWidget {
                     onPressed: () =>
                         context.read<TimerBloc>().add(const TimerPaused()),
                   ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
                 ],
               TimerRunPause() => [
                   FloatingActionButton(
                     child: const Icon(Icons.play_arrow),
                     onPressed: () =>
                         context.read<TimerBloc>().add(const TimerResumed()),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
                   ),
                 ],
               TimerRunComplete() => [
@@ -99,7 +111,7 @@ class Actions extends StatelessWidget {
                     onPressed: () =>
                         context.read<TimerBloc>().add(const TimerReset()),
                   ),
-                ],
+                ]
             }
           ],
         );
@@ -113,18 +125,33 @@ class Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Colors.blue.shade50,
-            Colors.blue.shade500,
+    return WaveWidget(
+      config: CustomConfig(
+        gradients: [
+          [
+            const Color.fromRGBO(72, 74, 126, 1),
+            const Color.fromRGBO(125, 170, 206, 1),
+            const Color.fromRGBO(184, 189, 245, 0.7)
           ],
-        )),
+          [
+            const Color.fromRGBO(72, 74, 126, 1),
+            const Color.fromRGBO(125, 170, 206, 1),
+            const Color.fromRGBO(172, 182, 219, 0.7)
+          ],
+          [
+            const Color.fromRGBO(72, 73, 126, 1),
+            const Color.fromRGBO(125, 170, 206, 1),
+            const Color.fromRGBO(190, 238, 246, 0.7)
+          ],
+        ],
+        durations: [19440, 10800, 6000],
+        heightPercentages: [0.03, 0.01, 0.02],
+        gradientBegin: Alignment.bottomCenter,
+        gradientEnd: Alignment.topCenter,
       ),
+      size: const Size(double.infinity, double.infinity),
+      waveAmplitude: 25,
+      backgroundColor: Colors.blue[50],
     );
   }
 }
