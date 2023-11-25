eec789479d691034de04fc4cfb2cb50255e68c58
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-25 17:03:36 +0300

    Update posts_list.dart

---
+++ lib/infinite_list_app/posts/view/posts_list.dart
@@ -0,0 +1,107 @@
+import 'package:bloc_patterns/infinite_list_app/posts/bloc/post_bloc.dart';
+import 'package:bloc_patterns/infinite_list_app/posts/bloc/post_event.dart';
+import 'package:bloc_patterns/infinite_list_app/posts/bloc/post_state.dart';
+import 'package:bloc_patterns/infinite_list_app/posts/models/models.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PostsList extends StatefulWidget {
+  const PostsList({super.key});
+
+  @override
+  State<PostsList> createState() => _PostsListState();
+}
+
+class _PostsListState extends State<PostsList> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostBloc, PostState>(
+      builder: (context, state) {
+        switch (state.status) {
+          case PostStatus.initial:
+            return const Center(child: CircularProgressIndicator());
+          case PostStatus.success:
+            if (state.posts.isEmpty) {
+              return const Center(
+                child: Text('no posts'),
+              );
+            }
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return index >= state.posts.length
+                    ? const BottomLoader()
+                    : PostListItem(post: state.posts[index]);
+              },
+              itemCount: state.hasReachedMax
+                  ? state.posts.length
+                  : state.posts.length + 1,
+              controller: _scrollController,
+            );
+          case PostStatus.failure:
+            return const Center(child: Text('failed to fetch posts'));
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom) context.read<PostBloc>().add(PostFetched());
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
+  }
+}
+
+class BottomLoader extends StatelessWidget {
+  const BottomLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 24,
+        width: 24,
+        child: CircularProgressIndicator(strokeWidth: 1.5),
+      ),
+    );
+  }
+}
+
+class PostListItem extends StatelessWidget {
+  const PostListItem({required this.post, super.key});
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Material(
+      child: ListTile(
+        leading: Text('${post.id}', style: textTheme.bodySmall),
+        title: Text(post.title),
+        isThreeLine: true,
+        subtitle: Text(post.body),
+        dense: true,
+      ),
+    );
+  }
+}
