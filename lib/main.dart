b589653966c309a8eff5f7122b560355a4fd5377
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-25 17:03:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:bloc_patterns/application_connecter.dart';
 import 'package:bloc_patterns/counter_app/app.dart';
+import 'package:bloc_patterns/infinite_list_app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'infinite_list_app/simple_bloc_observer.dart';
 
 ///[CounterApp]
 // void main() {
@@ -7,4 +12,9 @@ import 'package:flutter/material.dart';
 //   runApp(const CounterApp());
 // }
 
-void main() => runApp(const CounterApp());
+// void main() => runApp(const CounterApp());
+
+void main() {
+  Bloc.observer = const SimpleBlocObserver();
+  runApp(const ApplicationConnecter());
+}
