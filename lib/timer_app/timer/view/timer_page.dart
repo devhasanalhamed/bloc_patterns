9b219cf24c40a6729f8fe08fb22ab7dbba47c233
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-11-25 03:16:40 +0300

    Update timer_page.dart

--- lib/timer_app/timer/view/timer_page.dart
+++ lib/timer_app/timer/view/timer_page.dart
@@ -23,7 +23,7 @@ class TimerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Timer')),
+      appBar: AppBar(title: const Text('Timer')),
       body: const Stack(
         children: [
           Background(),
