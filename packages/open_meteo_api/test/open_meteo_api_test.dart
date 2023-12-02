f425fd8dd0994a2d72fc3a3ec35392ee3e30feea
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-02 18:23:30 +0300

    Update open_meteo_api_test.dart

--- packages/open_meteo_api/test/open_meteo_api_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -1,12 +1,26 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:open_meteo_api/open_meteo_api.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('Location', () {
+    group('fromJson', () {
+      test('returns correct Location object', () {
+        expect(
+          Location.fromJson(
+            <String, dynamic>{
+              'id': 4887398,
+              'name': 'Chicago',
+              'latitude': 41.85003,
+              'longitude': -87.65005,
+            },
+          ),
+          isA<Location>()
+              .having((w) => w.id, 'id', 4887398)
+              .having((w) => w.name, 'name', 'Chicago')
+              .having((w) => w.latitude, 'latitude', 41.85003)
+              .having((w) => w.longitude, 'longitude', -87.65005),
+        );
+      });
+    });
   });
 }
