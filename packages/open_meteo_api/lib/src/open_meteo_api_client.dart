9929edcd3cc0599d667edeca3ab3ad8e7c90a500
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-02 18:23:25 +0300

    Update open_meteo_api_client.dart

--- packages/open_meteo_api/lib/src/open_meteo_api_client.dart
+++ packages/open_meteo_api/lib/src/open_meteo_api_client.dart
@@ -55,11 +55,33 @@ class OpenMeteoApiClient {
   }
 
   ///Fetches [Weather] for a give [latitude] and [longitude].
-
   Future<Weather> getWeather({
     required double latitude,
     required double longitude,
   }) async {
-    throw UnimplementedError();
+    final weatherRequest = Uri.https(
+      _baseUrlWeather,
+      'v1/forecast',
+      {
+        'latitude': '$latitude',
+        'longitude': '$longitude',
+        'current_weather': 'true',
+      },
+    );
+
+    final weatherResponse = await _httpClient.get(weatherRequest);
+    if (weatherResponse.statusCode != 200) {
+      throw WeatherRequestFailure();
+    }
+
+    final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+
+    if (!bodyJson.containsKey('current_weather')) {
+      throw WeatherNotFoundFailure();
+    }
+
+    final weatherJson = bodyJson['current_weather'];
+
+    return Weather.fromJson(weatherJson);
   }
 }
