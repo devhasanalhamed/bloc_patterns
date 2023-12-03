896448a5ba8521d3b9783703a316183d048df698
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-12-03 09:30:33 +0300

    writing some unit test

--- packages/open_meteo_api/test/open_meteo_api_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -1,25 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mocktail/mocktail.dart';
 import 'package:open_meteo_api/open_meteo_api.dart';
-import 'package:test/test.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+class MockResponse extends Mock implements http.Response {}
+
+class FakeUri extends Fake implements Uri {}
 
 void main() {
-  group('Location', () {
-    group('fromJson', () {
-      test('returns correct Location object', () {
-        expect(
-          Location.fromJson(
-            <String, dynamic>{
-              'id': 4887398,
-              'name': 'Chicago',
-              'latitude': 41.85003,
-              'longitude': -87.65005,
-            },
-          ),
-          isA<Location>()
-              .having((w) => w.id, 'id', 4887398)
-              .having((w) => w.name, 'name', 'Chicago')
-              .having((w) => w.latitude, 'latitude', 41.85003)
-              .having((w) => w.longitude, 'longitude', -87.65005),
-        );
+  group('OpenMeteoApiClient', () {
+    late http.Client httpClient;
+    late OpenMeteoApiClient apiClient;
+
+    setUpAll(() {
+      registerFallbackValue(FakeUri());
+    });
+
+    setUp(() {
+      httpClient = MockHttpClient();
+      apiClient = OpenMeteoApiClient(httpClient: httpClient);
+    });
+
+    group('constructor', () {
+      test('does not require an httpClient', () {
+        expect(OpenMeteoApiClient(), isNotNull);
+      });
+    });
+
+    group('locationSearch', () {
+      const query = 'mock-query';
+      test('makes correct http request', () async {
+        final response = MockResponse();
+        when(() => response.statusCode).thenReturn(200);
+        when(() => response.body).thenReturn('{}');
+        when(() => httpClient.get(any())).thenAnswer((_) async => response);
+        try {
+          await apiClient.locationSearch(query);
+        } catch (_) {
+          verify(
+            () => httpClient.get(
+              Uri.https(
+                'geocoding-api.open-meteo.com',
+                '/v1/search',
+                {'name': query, 'count': '1'},
+              ),
+            ),
+          ).called(1);
+        }
       });
     });
   });
